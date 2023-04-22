e09dd384b728ac7fd414196d567ef3f7f4998dd8
panyanyany <568397440@qq.com>
2023-04-22 14:12:14 +0800

    正确显示status

--- app/Orchid/Layouts/OpenAiAccount/OpenAiAccountEditLayout.php
+++ app/Orchid/Layouts/OpenAiAccount/OpenAiAccountEditLayout.php
@@ -22,23 +22,23 @@ class OpenAiAccountEditLayout extends Rows {
             Input::make('open_ai_account.name')->type('text')->max(255)->title(__('名称'))->placeholder(__('Name')),
 
             Input::make('open_ai_account.email')
-                ->type('text')
-                ->required()
-                ->title(__('Email'))
-                ->placeholder(__('Email')),
+                 ->type('text')
+                 ->required()
+                 ->title(__('Email'))
+                 ->placeholder(__('Email')),
 
             Input::make('open_ai_account.api_key')
-                ->type('text')
-                ->required()
-                ->title(__('Api key'))
-                ->placeholder(__('api_key')),
+                 ->type('text')
+                 ->required()
+                 ->title(__('Api key'))
+                 ->placeholder(__('api_key')),
 
             Input::make('open_ai_account.password')->type('text')->title(__('密码'))->placeholder(__('password')),
             Input::make('open_ai_account.email_password')->type('text')->title(__('邮箱密码'))->placeholder(__('email_password')),
 
             Input::make('open_ai_account.usd_spent')->type('number')->title(__('消耗'))->placeholder(__('usd_spent'))->value(0),
             Input::make('open_ai_account.usd_spent_limit')->type('number')->title(__('最大消耗'))->placeholder(__('usd_spent_limit'))->value(0),
-            Select::make('open_ai_account.status')->options(OpenAiAccount::getStatuses())->title(__('状态'))->placeholder(__('status')),
+            Select::make('open_ai_account.status')->options(array_merge(['' => '请选择'], OpenAiAccount::getStatuses()))->title(__('状态'))->placeholder(__('status')),
             Input::make('open_ai_account.query_cnt')->type('number')->title(__('查询次数'))->placeholder(__('query_cnt'))->value(0),
             // Input::make('credit_used')->type('number')->title(__('credit_used'))->placeholder(__('credit_used')),
             // Input::make('credit_available')->type('number')->title(__('credit_available'))->placeholder(__('credit_available')),
